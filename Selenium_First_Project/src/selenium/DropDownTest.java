81f33c445cbd5763ce92ec4b23ea21b1f840de92
Hardik <hardik8784@gmail.com>
2022-10-14 16:16:26 -0400

    [Updated] DropDownTest Project fixed
    Changed the Driver from firefox to chrome

--- Selenium_First_Project/src/selenium/DropDownTest.java
+++ Selenium_First_Project/src/selenium/DropDownTest.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.support.ui.Select;
 
 public class DropDownTest {
@@ -12,10 +14,11 @@ public class DropDownTest {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.gecko.driver", "B:\\Hardik-Selenium\\QA\\SeleniumJars\\geckodriver.exe");
-		
+		System.setProperty("webdriver.chrome.driver", "B:\\Hardik-Selenium\\QA\\SeleniumJars\\chromedriver.exe");
 		//ClassName objName = new ClassName();
-		FirefoxDriver driver = new FirefoxDriver();		//This has constructor inside which opening the Firefox
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.ebay.ca/");	//This has constructor inside which opening the Firefox
 		
 		driver.get("https://www.ebay.ca/");
 		List<WebElement> optionsList = driver.findElements(By.xpath("//select[@id='gh-cat']/option"));
@@ -37,7 +40,7 @@ public class DropDownTest {
 		
 		driver.findElement(By.xpath("//div[@id='close']")).click();*/
 		
-		/*WebElement options_box = driver.findElement(By.id("gh-cat"));
+		WebElement options_box = driver.findElement(By.id("gh-cat"));
 		Select select_option = new Select(options_box);
 		
 		select_option.selectByIndex(1);		//Select Antiques
@@ -46,7 +49,7 @@ public class DropDownTest {
 		select_option.selectByValue("2984");	//Select Baby
 		Thread.sleep(3000);
 		
-		select_option.selectByVisibleText("Books");	//Select Books*/
+		select_option.selectByVisibleText("Books");	//Select Books
 	}
 
 }
