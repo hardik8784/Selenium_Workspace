ed15d3cd3d4ac95ed0c47da542b3184deac4b461
Hardik <hardik8784@gmail.com>
2023-04-19 12:50:52 -0400

    [Added] Soap XML Request with File I/O stream

---
+++ RESTAssuredAutomation/src/test/java/tests/SoapXMLRequest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import static io.restassured.RestAssured.baseURI;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+
+public class SoapXMLRequest {
+	
+	@Test
+	public void validateSoapXML() throws IOException {
+		
+		File file = new File("./SoapRequest/Add.xml");
+		
+		if(file.exists())
+		{
+			System.out.println("File is available,proceed further");
+		}
+		FileInputStream fileInputStream = new FileInputStream(file);
+		String requestBody = IOUtils.toString(fileInputStream, "UTF-8");
+		
+		
+		baseURI = "http://www.dneonline.com";
+		
+		RestAssured.given().contentType("text/xml").accept(ContentType.XML).body(requestBody).when().post("/calculator.asmx").then().statusCode(200).log().all();
+	}
+
+}
